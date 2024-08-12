766778e5a43ef1c626a2a7baeb94403a8c42c737
JAHID HASAN <developerjahid05@gmail.com>
2024-08-12 07:30:52 +0600

    express.Router()

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const userRouter = express_1.default.Router();
+app.use('/', userRouter);
+userRouter.get('/api/v1/user/create-user', (req, res) => {
+    const user = req.body;
+    res.send({ name: "jahid hasan" });
+});
 app.get('/', (req, res) => {
-    res.send('Hell ');
+    res.send('jahid the best ');
+});
+app.post('/', (req, res) => {
+    console.log('jahid :', req.body);
+    res.send('posting');
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
-import express from 'express'
+import express, { Request } from 'express'
 const app = express()
+
+app.use(express.json())
+
+
+const userRouter= express.Router()
+app.use('/', userRouter)
+userRouter.get('/api/v1/user/create-user', (req, res)=>{
+
+  const user= req.body;
+  res.send({name:"jahid hasan"})
+
+})
 
 
 app.get('/', (req, res) => {
-  res.send('Hell ')
+ 
+  res.send('jahid the best ')
+})
+app.post('/', (req, res)=>{
+    console.log('jahid :',req.body);
+
+    res.send('posting')
+
 })
 
 export default app;
